69210b1f540666bbb51a0d1e0a37698c8ea0c4a9
Harshit_Parmar <parmarharshit441@gmail.com>
2026-02-26 20:06:37 +0530

    add CORS support for Vercel deployment URL !

--- server/src/app.js
+++ server/src/app.js
@@ -38,7 +38,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://pixel-symbiosis-skill-hackathon.onrender.com"],
+    origin: ["http://localhost:3000", "https://pixel-symbiosis-skill-hackathon.onrender.com","https://pixel-symbiosis-skill-hackathon.vercel.app"],
     credentials: true,
   })
 );
